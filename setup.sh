69c197b695fbbbc4fe2fe58e8d4614496afd4716
Ishan Azad <ishansazad@gmail.com>
2023-10-24 17:45:21 -0700

    added collectstatic

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ set -o errexit
 
 ## Install dependencies
 pip install -r requirements.txt
+python manage.py collectstatic --no-input
 
 ## Run migrations in case any migrations hadn't been run yet
 python manage.py migrate
